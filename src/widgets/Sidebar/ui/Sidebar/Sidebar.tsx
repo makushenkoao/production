ea684c8223f24c3294c9aa1bba55a968bae4924c
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-05-27 22:50:18 +0300

    fix unit test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState<boolean>(false);
+    const { t } = useTranslation();
 
     const onToggle = (): void => setCollapsed((prevState) => !prevState);
 
@@ -20,7 +22,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 className,
             ])}
         >
-            {/* <button type="button" data-testid="sidebar-toggle" onClick={onToggle}>Toggle</button> */}
+            <button
+                type="button"
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                {t('Відкрити')}
+            </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher />
