8d68ce10a2629d20eafc7e808846125a9a883bb8
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-06-02 22:13:26 +0300

    add tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginIsLoading } from './getLoginIsLoading';
+
+describe('get login loading', () => {
+    test('should return boolean true', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                isLoading: true,
+            },
+        };
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(true);
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
+    });
+});
