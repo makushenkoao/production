9d850416c4c94b2107c0eed48ebb18aaf2568070
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-06-26 22:28:03 +0300

    add alias storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -17,6 +17,10 @@ export default ({ config }: {
     };
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
     // TODO fix types
     // @ts-ignore
     config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
