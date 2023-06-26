9aef197f4f0f33171d975757bfb3723d80949f29
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-06-27 01:34:54 +0300

    fix jest and webpack configs

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -58,6 +58,7 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
         axios: 'axios/dist/node/axios.cjs',
     },
 
