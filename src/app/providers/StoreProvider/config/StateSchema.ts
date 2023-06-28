62ab71070b5776e12c36112e0ff84b4cf0b87ad7
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-06-28 17:21:39 +0300

    remove unused imports

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -9,10 +9,7 @@ import { AxiosInstance } from 'axios';
 import { UserSchema } from '@/entities/User';
 import { LoginSchema } from '@/features/AuthByUsername';
 import { ArticleDetailsSchema } from '@/entities/Article';
-import {
-    ArticleDetailsCommentsSchema, ArticleDetailsPageSchema,
-    ArticleDetailsRecommendationsSchema,
-} from '@/pages/ArticleDetailsPage';
+import { ArticleDetailsPageSchema } from '@/pages/ArticleDetailsPage';
 import { AddCommentFormSchema } from '@/features/AddCommentForm';
 import { ArticlesPageSchema } from '@/pages/ArticlesPage';
 import { UISchema } from '@/features/UI';
