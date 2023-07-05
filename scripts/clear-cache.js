3ee2e7233be2afce19eb67023b9c43d5a828d761
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-07-05 15:01:03 +0300

    add script delete cache

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,1 +1,13 @@
-console.log('CLEAR CACHE')
+const fs = require('fs');
+const path = require('path');
+
+const nodeModulesPath = path.join(__dirname, 'node_modules');
+const cacheFolderPath = path.join(nodeModulesPath, '.cache');
+
+fs.rm(cacheFolderPath, { recursive: true }, (err) => {
+    if (err) {
+        console.error('Error deleting cache folder:', err);
+    } else {
+        console.log('Cache folder deleted successfully.');
+    }
+});
